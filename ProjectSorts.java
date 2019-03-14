4a6c5e18951d889ce5abd66fcdf05ecd569ea29a
xueenwu <45607584+xueenwu@users.noreply.github.com>
2019-03-14 15:13:18 -0400

    Add files via upload

---
+++ ProjectSorts.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+
+public class ProjectSorts 
+{
+	public static final int SIZE = 50;
+	
+	public static void main(String[] args)
+	{
+		int[] values = new int[50];
+		for (int i = 0; i < values.length; i++)
+		{
+			values[i] = (int) (Math.random() * 100);
+		}	
+		mergeSort(values, 0, values.length);
+		System.out.println(Arrays.toString(values));
+	}
+	
+	public static void testMergeSort()
+	{
+		int[] values = new int[SIZE];
+		for (int i = 0; i < values.length; i++)
+		{
+			values[i] = (int) (Math.random() * 100);
+		}	
+		mergeSort(values, 0, values.length);
+		System.out.println(Arrays.toString(values));
+	}
+	
+	public static void mergeSort(int[] array, int firstIndex, int lastIndex)
+	{
+		if (lastIndex - firstIndex > 1)
+		{
+			mergeSort(array, firstIndex, (firstIndex + lastIndex + 1) / 2);
+			mergeSort(array, (firstIndex + lastIndex + 1) / 2, lastIndex);
+			merge(array, firstIndex, (firstIndex + lastIndex + 1) / 2, (firstIndex + lastIndex + 1) / 2, lastIndex);
+		}
+	}
+	
+	private static void merge(int[] array, int leftFirst, int leftLast, int rightFirst, int rightLast)
+	{
+			int[] tempArray = new int[array.length];
+			int saveFirst = leftFirst;
+			int index = leftFirst;
+			while (leftFirst < leftLast && rightFirst < rightLast)
+			{
+				if (array[leftFirst] < array[rightFirst])
+				{
+					tempArray[index] = array[leftFirst];
+					leftFirst++;
+				}
+				else
+				{
+					tempArray[index] = array[rightFirst];
+					rightFirst++;
+				}
+				index++;
+			}
+			while (leftFirst < leftLast)
+			{
+				tempArray[index] = array[leftFirst];
+				leftFirst++;
+				index++;
+			}
+			while (rightFirst < rightLast)
+			{
+				tempArray[index] = array[rightFirst];
+				rightFirst++;
+				index++;
+			}
+			for (int i = saveFirst; i < rightLast; i++)
+			{
+				array[i] = tempArray[i];
+			}
+	}
+}
