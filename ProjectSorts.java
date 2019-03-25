468d759f8bc01dabc78c9a0ff38a5168939203e0
xueenwu <45607584+xueenwu@users.noreply.github.com>
2019-03-24 21:24:58 -0400

    Add files via upload

--- ProjectSorts.java
+++ ProjectSorts.java
@@ -2,11 +2,9 @@ public class ProjectSorts
 {
 	public static int MAX_NUMBER = 100;
 	public static int MIN_NUMBER = 0;
-	public static int[] SIZES = {
-//			5, 10, 20, 30, 40, 50, 100, 200, 300, 400, 500, 1000, 
-//			2000, 3000, 4000, 5000, 10000, 20000, 30000, 40000, 50000, 100000
-			60000, 70000, 80000, 90000
-			};
+	public static int[] SIZES = {5, 10, 20, 30, 40, 50, 100, 200, 300, 
+			400, 500, 1000, 2000, 3000, 4000, 5000, 10000, 20000, 30000, 
+			40000, 50000, 60000, 70000, 80000, 90000, 100000};
 	public static int TRIALS = 30;
 	
 	/**
